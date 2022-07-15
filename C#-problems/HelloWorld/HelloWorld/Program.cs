7fa83171c650290a8c48752122e1d2e859f9f1ae
Kiril Gradinarov <kiril.gradinarov@grafixoft.com>
2022-07-15 16:46:58 +0300

    Fixed new formating isues

--- C#-problems/HelloWorld/HelloWorld/Program.cs
+++ C#-problems/HelloWorld/HelloWorld/Program.cs
@@ -7,80 +7,59 @@ class HelloWorld
 
     static void DateCalculations()
     {
-
         string input = Console.ReadLine();
         DateTime date = DateTime.ParseExact(input, "dd-mm-yyyy", CultureInfo.InvariantCulture);
 
         date = date.AddDays(bonusDays);
         Console.WriteLine(date.ToString("dd-mm-yyyy"));
-
     }
 
     static void DateCalculationsFromCurrentDate()
     {
-
-        var date = DateTime.Now;
+        DateTime date = DateTime.Now;
 
         date = date.AddDays(bonusDays);
         Console.WriteLine(date.ToString("dd-mm-yyyy"));
-
     }
 
     static void PrintInfo(string firstName, string lastName, int age, string town)
     {
-
         const string boilerplateString = "You are {0} {1}, a {2}-years old person from {3}";
 
         Console.WriteLine(boilerplateString, firstName, lastName, age, town);
-
     }
 
     static void PrintRectangleStars(int number)
     {
-
         for (int i = 0; i < number; i++)
         {
-
             if (i == 0 || i == number - 1)
             {
-
                 for (int j = 0; j < number; j++)
                 {
-
                     Console.Write('*');
-
                 }
-
             }
 
             else
             {
-
                 for (int j = 0; j < number; j++)
                 {
-
                     if (j == 0 || j == number - 1)
                     {
-
                         Console.Write('*');
-
                     }
-
                     else
                     {
-
                         Console.Write(' ');
-
                     }
-
                 }
-
             }
 
             Console.WriteLine();
-
         }
     }
+
     static void Main()
     {
         DateCalculations();
@@ -88,7 +67,6 @@ class HelloWorld
 }
 public class MyConverter
 {
-    const double BGNtoBGNMultiplier = 1;
     const double USDtoBGNMultiplier = 1.79549;
     const double EURtoBGNMultiplier = 1.95583;
     const double GBPtoBGNMultiplier = 2.53405;
@@ -96,39 +74,32 @@ public class MyConverter
     const double CelsiusToFarenheitMultiplier = 1.8;
     const double CelsiusToFarenheitOfset = 32.0;
 
-    const double RadiansToDegreesMultiplier = 180;
+    const double RadiansToDegreesMultiplier = 180.0;
 
 
     public double FromCelsiusToFarenheit(double C)
     {
-
         double F = C * CelsiusToFarenheitMultiplier + CelsiusToFarenheitOfset;
 
         return Math.Round(F, 2);
-
     }
 
     public double FromRadiansToDegrees(double R)
     {
-
         double D = R / Math.PI * RadiansToDegreesMultiplier;
 
         return Math.Round(D, 0);
-
     }
 
     public double FromUSDtoBGN(double USD)
     {
-
         double BGN = USD * USDtoBGNMultiplier;
 
         return Math.Round(BGN, 2);
-
     }
 
     public double CurrencyConverter(double amount, string originalCurrency, string newCurrency)
     {
-
         double inBGN;
         double newAmount;
 
@@ -181,7 +152,6 @@ public class MyConverter
         }
 
         return Math.Round(newAmount, 2);
-
     }
 }
 
@@ -196,6 +166,7 @@ public class MyMath
         Console.WriteLine("Triangle area = {0}", area);
 
     }
+
     public void RectangleAreaAndPerimeter(double x1, double y1, double x2, double y2)
     {
 
@@ -208,6 +179,7 @@ public class MyMath
         Console.WriteLine("Perimeter = {0}", perimeter);
 
     }
+
     public void CircleAreaAndPerimeter(double r)
     {
 
@@ -218,18 +190,21 @@ public class MyMath
         Console.WriteLine("Perimeter = {0}", perimeter);
 
     }
+
     public double TrapezoidArea(double b1, double b2, double h)
     {
 
         return ((b1 + b2) / 2) * h;
 
     }
+
     public int SqureArea(int a)
     {
 
         return a * a;
 
     }
+
     public double InchesToCm(double a)
     {
 
